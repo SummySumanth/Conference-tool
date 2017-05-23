e02fbb97e8a88d959bffe9e43b4b49f4edc81905
Sumanth <sumanth261094@gmail.com>
2017-05-23 10:12:39 +0530

    track approval card

--- js/pages/generalScript.js
+++ js/pages/generalScript.js
@@ -5,4 +5,9 @@ let homepage = "/works/Conference-tool";
 let toastIT = (message, duration) =>{
     var $toastContent = $('<span >' + message + '</span>');
     Materialize.toast($toastContent, duration);
-}
+};
+
+$('#backBtn').on('click', ()=>{
+    console.log('back button clicked');
+    window.history.back();
+});
